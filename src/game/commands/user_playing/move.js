d3d6d9eedca2d5d92c9ea2a5f191923621c5fee5
Harrison Hutcheon <harrison.hutcheon@threepointturn.com>
2019-04-04 11:50:28 -0400

    Move working

--- src/game/commands/user_playing/move.js
+++ src/game/commands/user_playing/move.js
@@ -1,14 +1,42 @@
-import { DIR } from '../../constants/numbers'
+import { DIR, MAP_SIZE } from '../../constants/numbers'
 import { STOPPED, MOVING } from '../../constants/states'
 
 
-const move = ({ payload, socket, io, user, rooms }) => {
+const move = ({ payload, socket, io, user }) => {
   const dir = payload.options.find(o => o.option === 'dir').values[0]
   const dist = payload.options.find(o => o.option === 'dist').values[0]
   const direction = DIR[dir]
+  let moveCounter = 0
   user.player_state = MOVING
   user.moveTimer = setInterval(() => {
+    const newPos = {
+      x: user.pos.x + direction.x,
+      y: user.pos.y + direction.y,
+    }
+    const oldPos = { ...user.pos }
 
+    // if new pos is outside the limits of the map
+    if (newPos.x >= MAP_SIZE - 1 || newPos.y >= MAP_SIZE - 1) {
+      // stop, inform the player
+    } else {
+      user.pos = newPos
+      io.to(socket.id)
+        .emit(
+          'notification',
+          { msg: `You moved one tile. (${oldPos.x}, ${oldPos.y}) to (${newPos.x}, ${newPos.y})` },
+        )
+      moveCounter += 1
+      if (moveCounter === dist) {
+        clearInterval(user.moveTimer)
+        user.moveTimer = undefined
+        user.player_state = STOPPED
+        io.to(socket.id)
+          .emit(
+            'notification',
+            { msg: `Move complete. Current Position: (${user.pos.x}, ${user.pos.y})` },
+          )
+      }
+    }
   }, user.speed)
 }
 
